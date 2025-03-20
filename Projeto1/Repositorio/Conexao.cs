b92bbb6bee5ec202b462d4718a6ab04fbd1b7c9d
Paulo <irineu32323@gmail.com>
2025-03-20 15:32:28 -0300

    Adicionando usuario repositório e conexão

--- Projeto1/Repositorio/Conexao.cs
+++ Projeto1/Repositorio/Conexao.cs
@@ -1,6 +1,32 @@
-﻿namespace Projeto1.Repositorio
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Projeto1.Repositorio
 {
-    public class Conexao
+    public class Conexao : IDisposable
     {
+
+        private MySqlConnection _connection;
+
+        public Conexao(string connectionString)
+        {
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        public MySqlCommand MySqlCommand()
+        {
+            return _connection.CreateCommand();
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null && _connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+        }
     }
 }
--- Projeto1/Repositorio/UsuarioRepositorio.cs
+++ Projeto1/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,59 @@
-﻿namespace Projeto1.Repositorio
+﻿using Projeto1.Models;
+
+namespace Projeto1.Repositorio
 {
     public class UsuarioRepositorio
     {
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+        public Usuario ObterUsuario(string email)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "SELECT * FROM Usuario WHERE Email = @Email";
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.ExecuteNonQuery();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Usuario
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Nome = reader.GetString("Nome"),
+                            Email = reader.GetString("Email"),
+                            Senha = reader.GetString("Senha"),
+
+                        };
+
+                    }
+
+                }
+                return null;
+            }
+        }
     }
+
+
 }
+
