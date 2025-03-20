f206ed3987aaca11e77300b9ceaaa05228a097f0
Paulo <irineu32323@gmail.com>
2025-03-20 14:56:20 -0300

    Consulta de Login

--- Projeto1/Controllers/LoginController.cs
+++ Projeto1/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projeto1.Controllers
 {
@@ -6,6 +7,21 @@ namespace Projeto1.Controllers
     {
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if(usuario != null && usuario.Senha == senha)
+            {
+                //Autenticação bem-sucedida
+                // return RedirectToAction("Index","Home"); a caso esteja errado ter como trocar rapido.
+                // ele retorna se tiver certo da página Login e redireciona para a pagina Home ("Login",depois "Home")
+                return RedirectToAction("Login", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senhas inválidos.");
             return View();
         }
     }
